84c67f3b7b1a14214dc5c58f739065021f54f322
jorge <joscartin11@gmail.com>
2025-11-06 20:02:24 +0100

    no se como llamarlo, bien podria ser el principal

---
+++ herramientas-1.c
@@ -0,0 +1,23 @@
+void normalizar (double *datos, int ndatos){
+    int i;
+    float maximo, minimo;
+    maximo=datos[0];
+    minimo=datos[0];
+    for (i=0;i<ndatos;i++){
+        if (datos[i]>maximo){
+            maximo=datos[i];
+        } else if (datos[i]<minimo){
+            minimo=datos[i];
+        }
+    }
+    for (i=0;i<ndatos;i++){
+        datos[i]=(datos[i]-minimo)/(maximo-minimo);
+    }
+}
+
+void ENN (){
+}
+
+void knn (){
+    
+}
